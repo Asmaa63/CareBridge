174b11abdc2115f09f832d437549d38cbcf9aa2f
Asmaa63 <semoasmaa333@gmail.com>
2025-02-12 19:53:51 +0200

    edit link and add vercel.json

--- src/Components/Header/Footer.tsx
+++ src/Components/Header/Footer.tsx
@@ -39,7 +39,7 @@ const Footer: React.FC = () => {
 };
 
     try {
-      const response = await fetch("https://carebridger.runasp.net/Contact/submitcontact", {
+      const response = await fetch("/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
--- src/Components/Pages/ContactUs.tsx
+++ src/Components/Pages/ContactUs.tsx
@@ -32,7 +32,7 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   };
 
   try {
-    const response = await fetch("https://carebridger.runasp.net/Contact/submitcontact", {
+    const response = await fetch("/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
